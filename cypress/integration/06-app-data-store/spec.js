72af1ce265e727374ad66cc6c2f77360b9a38276
Jeroen Engels <jfm.engels@gmail.com>
2018-06-29 17:59:30 +0200

    Step 6

--- cypress/integration/06-app-data-store/spec.js
+++ cypress/integration/06-app-data-store/spec.js
@@ -5,10 +5,9 @@ beforeEach(() => {
   cy.request('POST', '/reset', {
     todos: []
   })
-})
-beforeEach(() => {
   cy.visit('/')
 })
+
 /**
  * Adds a todo item
  * @param {string} text
@@ -20,12 +19,13 @@ it('creates an item with id 1', () => {
   cy.server()
   cy.route('POST', '/todos').as('new-item')
 
-  // TODO change Math.random to be deterministic
-
   // STEPS
   // get the application's "window" object using cy.window
   // then change its Math object and replace it
   // with your function that always returns "0.1"
+  cy.window().its('Math').then(Math => {
+    Math.random = () => 0.1
+  })
 
   addItem('something')
   // confirm the item sent to the server has the right values
@@ -38,6 +38,11 @@ it('creates an item with id 1', () => {
 
 // stub function Math.random using cy.stub
 it('creates an item with id using a stub', () => {
+  cy.window().its('Math').then(Math => {
+    cy.stub(Math, 'random', () => 0.2).as('math-random')
+  })
+  addItem('foo')
+  cy.get('@math-random').should('have.been.calledOnce')
   // get the application's "window.Math" object using cy.window
   // replace Math.random with cy.stub and store the stub under an alias
   // create a todo using addItem("foo")
@@ -45,9 +50,28 @@ it('creates an item with id using a stub', () => {
 })
 
 it('puts the todo items into the data store', () => {
+  let index = 1
+  cy.window().its('Math').then(Math => {
+    Math.random = () => `0.${index++}`
+  })
+
+  addItem('something')
+  addItem('else')
   // application uses data store to store its items
   // you can get the data store using "window.app.$store.state.todos"
   // add a couple of items
   // get the data store
   // check its contents
+  cy.window().its('app.$store.state.todos').should('deep.equal', [
+    {
+      id: '1',
+      title: 'something',
+      completed: false
+    },
+    {
+      id: '2',
+      title: 'else',
+      completed: false
+    }
+  ]);
 })
