5effb6b0a0f2d4d08dd0c5aa1b900a5170cd4f90
Jeroen Engels <jfm.engels@gmail.com>
2018-06-29 16:17:36 +0200

    Step 1

--- cypress/integration/01-basic/spec.js
+++ cypress/integration/01-basic/spec.js
@@ -2,8 +2,8 @@
 // @ts-check
 it('loads', () => {
   // application should be running at port 3000
-  cy.visit('localhost:3000')
+  cy.visit('/')
   // this assertion fails on purpose
   // can you fix it?
-  cy.contains('Part of TodoMVC')
+  cy.contains('a', 'TodoMVC')
 })
